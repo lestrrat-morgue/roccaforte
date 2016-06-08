a6bdfb14359ca9d4b8f139f36c0fa589600104a3
Daisuke Maki <lestrrat+github@gmail.com>
2016-06-08 10:27:10 +0900

    Delete item on defer

--- incoming/gdatastore.go
+++ incoming/gdatastore.go
@@ -15,8 +15,12 @@ func NewGDatastoreStorage(projectID string) *GDatastoreStorage {
 	}
 }
 
+func (s *GDatastoreStorage) client(ctx context.Context) (*datastore.Client, error) {
+	return datastore.NewClient(ctx, s.ProjectID)
+}
+
 func (s *GDatastoreStorage) Save(ctx context.Context, events ...*ReceivedEvent) error {
-	cl, err := datastore.NewClient(ctx, s.ProjectID)
+	cl, err := s.client(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to create datastore client")
 	}
@@ -33,6 +37,29 @@ func (s *GDatastoreStorage) Save(ctx context.Context, events ...*ReceivedEvent) 
 		_, err := cl.Put(ctx, key, e)
 		if err != nil {
 			return errors.Wrap(err, "failed to Put event to datastore")
+		}
+	}
+	return nil
+}
+
+func (s *GDatastoreStorage) Delete(ctx context.Context, events ...*ReceivedEvent) error {
+	cl, err := s.client(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create datastore client")
+	}
+
+	// classify entries into basetime / event name
+	for _, e := range events {
+		id := e.ReceivedOn().Unix()
+		if mod := id % 300; mod > 0 {
+			id = id - mod
+		}
+
+		parent := datastore.NewKey(ctx, "ReceivedEvents", strconv.FormatInt(id, 10), 0, nil)
+		key := datastore.NewIncompleteKey(ctx, e.Name(), parent)
+		_, err := cl.Delete(ctx, key)
+		if err != nil {
+			return errors.Wrap(err, "failed to Delete event from datastore")
 		}
 	}
 	return nil
--- incoming/gdatastore_test.go
+++ incoming/gdatastore_test.go
@@ -1,13 +1,30 @@
 package incoming_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/lestrrat/roccaforte/incoming"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
 
+var projectID string
+
+func init() {
+	projectID = os.Getenv("DATASTORE_PROJECT_ID")
+}
+
 func TestGDatastore(t *testing.T) {
-	s := incoming.NewGDatastoreStorage("")
-	s.Save(context.Background(), incoming.NewEvent(nil, "test.notify"))
+	if projectID == "" {
+		t.Skip("missing project ID. please set DATASTORE_PROJECT_ID")
+		return
+	}
+
+	s := incoming.NewGDatastoreStorage(projectID)
+	e := incoming.NewEvent(nil, "test.notify")
+	if !assert.NoError(t, s.Save(context.Background(), e), "s.Save should succeed") {
+		return
+	}
+	defer s.Delete(e)
 }
